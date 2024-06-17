244ab12a8f357d090716cc3334942accf164c630
h3x-hex <160248326+h3x-hex@users.noreply.github.com>
2024-06-17 11:58:16 +0530

    Update LoginUser.tsx

--- components/LoginUser.tsx
+++ components/LoginUser.tsx
@@ -59,7 +59,7 @@ export default function LoginUser () {
                 <div className="flex flex-col gap-4">
                         <input 
                                 type="text" 
-                                className="input input-bordered w-full"
+                                className="input input-bordered w-full text-gray-950"
                                 placeholder="Email"
                                 onChange={(e: any) => {
                                     setData({
@@ -70,7 +70,7 @@ export default function LoginUser () {
                             />
                             <input 
                                 type="password" 
-                                className="input input-bordered w-80"
+                                className="input input-bordered w-80 text-gray-950"
                                 placeholder="Password"
                                 onChange={(e: any) => {
                                     setData({
